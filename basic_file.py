a300d46d14df94e6c852186caf436f0fada2036a
przemek <bprzywara11@gmai.com>
2024-10-22 12:00:04 +0200

    test

---
+++ basic_file.py
@@ -0,0 +1,14 @@
+#2 v1
+def print_names(names):
+  for x in names:
+    print(x)
+
+names = ["a", "b", "c"]
+print_names(names)
+
+#2 v2
+print(names)
+
+names = ["a", "b", "c"]
+print_names(names)
+
