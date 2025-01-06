fdff5d355f2e7cb5cd767d1aaf3c6e45cab304d9
przemek <bprzywara11@gmai.com>
2025-01-06 14:02:59 +0100

    pliki

--- producer.py
+++ producer.py
@@ -1,27 +1,29 @@
-import csv
+import sqlite3
 import uuid
 
-FILE_NAME = 'tasks.csv'
+DB_NAME = 'tasks.db'
 
 def add_task():
     task_id = str(uuid.uuid4())
     task_status = 'pending'
-    task = [task_id, task_status]
 
-    # Sprawdzenie, czy plik istnieje i nagłówki są zapisane
-    try:
-        with open(FILE_NAME, 'r') as f:
-            pass
-    except FileNotFoundError:
-        with open(FILE_NAME, 'w', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(['id', 'status'])
 
-    # Dodanie nowego zadania
-    with open(FILE_NAME, 'a', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(task)
-        print(f"Added task with ID: {task_id}")
+    conn = sqlite3.connect(DB_NAME)
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS tasks (
+        id TEXT PRIMARY KEY,
+        status TEXT NOT NULL
+    )
+    """)
+
+    cursor.execute("INSERT INTO tasks (id, status) VALUES (?, ?)", (task_id, task_status))
+    conn.commit()
+
+    print(f"dodano ID: {task_id}")
+
+    conn.close()
 
 if __name__ == "__main__":
     add_task()
